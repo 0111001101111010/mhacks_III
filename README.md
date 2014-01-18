b73a21af5444339a6f3a6495f60251d8b75ec197
stanzheng <dev.stanzheng@gmail.com>
2014-01-18 01:47:04 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-mhacks_III
-==========
+Google Mirror API's Quick Start for PHP
+========================
+
+The documentation for this quick start is maintained on developers.google.com.
+Please see here for more information:
+https://developers.google.com/glass/quickstart/php
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright (C) 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//  Author: Jenny Murphy - http://google.com/+JennyMurphy
+
+// TODO: You must configure these fields for the starter project to function.
+// Visit https://developers.google.com/glass/getting-started to learn more
+$api_client_id = "59840552989-in9c4nhs59u5ammblfolv2qu16ira823.apps.googleusercontent.com";
+$api_client_secret = "jqORcnZA9Sq2qUM8u_xBt2LG";
+$api_simple_key = "AIzaSyCQpcwRX2oZxhYfZh0hTTLd1sQjQDZylU8";
+
+$base_url = "http://mateoj.com/mirror-php";
+
+// This should be writable by your web server's user
+$sqlite_database = "/tmp/database.sqlite";
